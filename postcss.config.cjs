4ce48e1def562e0cafcacf2a853f3baaa646e91e
Vyacheslav Bychkov <vyacheslav.bychkov@noveogroup.com>
2022-09-13 20:54:07 -0700

    feat: config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,12 +3,19 @@ module.exports = {
   plugins: {
     'postcss-functions': {
       functions: {
-        rem: function(pixVal) {
+        rem: function (pixVal) {
           const defaultRem = 16;
-          const unit = pixVal /  defaultRem;
+          const unit = pixVal / defaultRem;
           return unit + 'rem';
         }
       }
+    },
+    'postcss-advanced-variables': {
+      variables: {
+        'tablet-screen': '768px',
+        'desktop-screen': '1440px',
+      }
+
     },
     'autoprefixer': {},
     'postcss-nesting': {},
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 7007,
+  },
   plugins: [react()],
   css: {
+    /* eslint-disable */
     devSourcemap: true,
+    preprocessorOptions: {
+      postcss: {
+        additionalData: `@import "./src/assets/styles/helpers/index.postcss";`,
+      },
+      /* eslint-enable */
+    },
   }
 });
